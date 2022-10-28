31f65e2cd4c4297fb3408330660c39268944e903
4637-hafsa <hafsanoor401@gmail.com>
2022-10-28 12:48:14 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,15 +13,17 @@ char *_strncat(char *dest, char *src, int n)
 int c, i;
 c = 0;
 /*find size of dest array*/
-while (dest[c])
+while (dest[c] != '\0')
+{
 c++;
-/**
-* src does not need to be null terminated
-* if it contains n or more bytes
-*/
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[c + i] = src[i];
-/*null terminate dest*/
-dest[c + i] = '\0';
+}
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[c] = src[i];
+c++;
+i++;
+}
+dest[c] = '\0';
 return (dest);
 }
